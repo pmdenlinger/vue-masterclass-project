bf29df3d6ffd4ef7c13c75e5b7b7ebf7ad90dd28
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-25 13:12:04 -0700

    Add style to App.vue with the color purple property

--- job-search/src/App.vue
+++ job-search/src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="wrapper">
+    <h1>Hello, I'm in App.vue!</h1>
     <HelloWorld msg="You did it!" />
   </div>
 </template>
 
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 </script>
+
+<style>
+h1 {
+  color: purple;
+}
+</style>
