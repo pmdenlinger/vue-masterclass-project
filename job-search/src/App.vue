7285effa9b3324dce3f18bba6741d7a5119c27f7
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-25 13:18:54 -0700

    Delete HelloWorld.vue and all imports and references to it in App.vue

--- job-search/src/App.vue
+++ job-search/src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="wrapper">
     <h1>Hello, I'm in App.vue!</h1>
-    <HelloWorld msg="You did it!" />
+
   </div>
 </template>
 
 <script setup>
-import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <style scoped>
