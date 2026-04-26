c6d55f8ab07b7a6bade89a68ca3aa08e5b52860c
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-25 18:28:24 -0700

    Register MainNav child component in App.vue so that it displays in browser, using OptionsAPI shorthand

--- job-search/src/App.vue
+++ job-search/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="wrapper">
-    <h1 class="mt-8 text-6xl text-center">Hello Vue!</h1>
-
-  </div>
+  <MainNav />
 </template>
 
-<script setup>
+<script>
+import MainNav from '@/components/MainNav.vue'
+
+export default {
+  name: "App",
+  components: {
+    MainNav,
+  },
+};
 </script>
 
-<style scoped>
-h1 {
-  color: purple;
-}
-</style>
