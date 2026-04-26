e679e29b6d4782bf9c6498e3855c0d2633c8693d
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-25 19:20:16 -0700

    Add first data property to MainNav.vue

--- job-search/src/components/MainNav.vue
+++ job-search/src/components/MainNav.vue
@@ -3,7 +3,7 @@
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div
         class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
-        <a href="/" class="flex h-full items-center text-xl">Bobo Careers</a>
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
@@ -12,5 +12,10 @@
 <script>
 export default {
   name: 'MainNav',
+  data() {
+    return {
+      company: "Bobo Careers",
+    }
+  }
 }
 </script>
