04ba3b12410089317927ea55b50252698ef0a772
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-27 11:31:33 -0700

    Delete extra li items in MainNav.vue

--- job-search/src/components/MainNav.vue
+++ job-search/src/components/MainNav.vue
@@ -1,41 +1,14 @@
 <template>
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
-      <div
-        class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
+      <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
-            <li class="h-full">
-              <a href="/" class="flex h-full items-center py-2.5">Teams</a>
-            </li>
-
             <li class="ml-9 h-full">
               <a href="/" class="flex h-full items-center py-2.5">Locations</a>
             </li>
-
-            <li class="ml-9 h-full">
-              <a href="/" class="flex h-full items-center py-2.5">Life at Bobo Corp</a>
-            </li>
-
-            <li class="ml-9 h-full">
-              <a href="/" class="flex h-full items-center py-2.5">How we hire</a>
-            </li>
-
-            <li class="ml-9 h-full">
-              <a href="/" class="flex h-full items-center py-2.5">Students</a>
-            </li>
-
-            <li class="ml-9 h-full">
-              <a href="/" class="flex h-full items-center py-2.5">Jobs</a>
-            </li>
-
-
-
-
-
           </ul>
-
         </nav>
       </div>
     </div>
@@ -47,17 +20,10 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: "Bobo Careers",
-      url: "https://careers.google.com",
-      menuItems: [
-        "Teams",
-        "Locations",
-        "Life at Bobo Corp",
-        "How we hire",
-        "Students",
-        "Jobs",
-      ]
+      company: 'Bobo Careers',
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Locations', 'Life at Bobo Corp', 'How we hire', 'Students', 'Jobs'],
     }
-  }
+  },
 }
 </script>
