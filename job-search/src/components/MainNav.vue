f1835238991a786c3c4a357b68a0ae08687a008b
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-27 11:24:31 -0700

    Add tailwind styling to navbar itemswith ul, li tags

--- job-search/src/components/MainNav.vue
+++ job-search/src/components/MainNav.vue
@@ -4,6 +4,39 @@
       <div
         class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li class="h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Teams</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Locations</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Life at Bobo Corp</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">How we hire</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Students</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="/" class="flex h-full items-center py-2.5">Jobs</a>
+            </li>
+
+
+
+
+
+          </ul>
+
+        </nav>
       </div>
     </div>
   </header>
