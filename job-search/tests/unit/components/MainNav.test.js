eb4ba4f27f1ab659153e7154bf3558ef4f6b89ce
Paul Denlinger <pmdenlinger@icloud.com>
2026-05-02 10:55:09 -0700

    Rewrite test to check that ProfileImage does not yet show in test

--- job-search/tests/unit/components/MainNav.test.js
+++ job-search/tests/unit/components/MainNav.test.js
@@ -1,6 +1,8 @@
 import { mount } from '@vue/test-utils'
 
 import MainNav from '@/components/MainNav.vue'
+import ProfileImage from '@/components/ProfileImage.vue'
+import ActionButton from '@/components/ActionButton.vue'
 
 describe('MainNav', () => {
   it('displays company name', () => {
@@ -8,4 +10,34 @@ describe('MainNav', () => {
 
     expect(wrapper.text()).toContain('Bobo Careers')
   })
+
+  it('displays navigation menu items', () => {
+    const wrapper = mount(MainNav)
+    const navigationItems = ['Teams', 'Locations', 'Life at Bobo Corp', 'How we hire', 'Students', 'Jobs']
+
+    navigationItems.forEach((item) => {
+      expect(wrapper.text()).toContain(item)
+    })
+  })
+
+  describe('when the user is not logged in', () => {
+    it('displays action button', () => {
+      const wrapper = mount(MainNav)
+
+      expect(wrapper.findComponent(ActionButton).exists()).toBe(true)
+      expect(wrapper.findComponent(ProfileImage).exists()).toBe(false)
+    })
+  })
+
+  describe('when the user logs in', () => {
+    it('displays user profile picture', async () => {
+      const wrapper = mount(MainNav)
+
+      await wrapper.vm.loginUser()
+
+      expect(wrapper.findComponent(ProfileImage).exists()).toBe(true)
+      expect(wrapper.findComponent(ActionButton).exists()).toBe(false)
+    })
+  })
 })
+
