0bc9132b5d1a156d2c02d6ef87eb886815f28ce1
Paul Denlinger <pmdenlinger@icloud.com>
2026-05-02 12:53:35 -0700

    Create first prop in ActionButton.vue holding text property, then add Vue expression in MainNav.vue, and pass text to ActionButton.vue

--- job-search/src/components/ActionButton.vue
+++ job-search/src/components/ActionButton.vue
@@ -2,13 +2,14 @@
   <button
     class="rounded border-0 px-5 py-3 font-medium text-white bg-brand-blue-1 hover:shadow-blue"
   >
-    Sign in
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: 'ActionButton',
+  props: ["text"],
 
 }
 </script>
