2ad6d158d624bffc64d732b1a748827a3370ed65
Paul Denlinger <pmdenlinger@icloud.com>
2026-05-03 08:27:10 -0700

    Add tailwind styling for TheSubnav component

--- job-search/src/components/ActionButton.vue
+++ job-search/src/components/ActionButton.vue
@@ -21,7 +21,7 @@ export default {
       }
 
     }
-  }
+  },
   computed: {
     buttonClass() {
       return {
--- job-search/src/components/TheSubnav.vue
+++ job-search/src/components/TheSubnav.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>This is a subnav</div>
+  <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
+    <div class="flex h-full items-center px-8">This is a subnav</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "The Subnav",
+  name: 'The Subnav',
 }
 </script>
