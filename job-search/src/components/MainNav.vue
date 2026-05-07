15a5da4bd7bdfc5e715f8a8a5301ccde01ad1d48
Paul Denlinger <pmdenlinger@icloud.com>
2026-05-07 13:14:30 -0700

    Add headerHeightClass as computed property, then connect it to the header in template

--- job-search/src/components/MainNav.vue
+++ job-search/src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full text-sm">
+  <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
@@ -38,6 +38,14 @@ export default {
       url: 'https://careers.google.com',
       menuItems: ['Teams', 'Locations', 'Life at Bobo Corp', 'How we hire', 'Students', 'Jobs'],
       isLoggedIn: false,
+    }
+  },
+  computed: {
+    headerHeightClass() {
+      return {
+        "h-16": !this.isLoggedIn,
+        "h-32": this.isLoggedIn,
+      }
     }
   },
   methods: {
